085d5d0072f89c2e6846607b434148e81d8c9f80
Ivan Yelizariev <yelizariev@it-projects.info>
2015-08-14 11:56:21 +0500

    [DOC] move description to README.rst file

--- purchase_delivery/__openerp__.py
+++ purchase_delivery/__openerp__.py
@@ -24,27 +24,6 @@
     'name': 'Purchase Delivery Costs',
     'version': '1.0',
     'category': 'Purchase Management',
-    'description': """
-Allows you to add delivery methods in purchase orders and picking.
-==============================================================
-
-This module makes it possible to add delivery method to purchase orders and
-can compute a shipping line in the purchase order or in the invoice when
-created from an incoming shipment.
-
-The application makes use of the same concepts of carrier and delivery grid
-extending them as follows:
-    * Introduces origin countries, states and ZIP in the delivery grid.
-    * Uses the cost defined in the grid, instead of the sale price.
-    * Uses the vendor address to match with the existing grid based on the
-    origin information defined in the grid.
-    * Uses the destination address (customer address for direct shipments
-    and warehouse otherwise) to determine the grid, based on the destination
-    parameters defined in the grid.
-
-In case that the incoming shipment contains moves that have multiple
-destination addresses, computes the average freight cost.
-""",
     'author': 'Eficent',
     'depends': ['delivery', 'purchase'],
     'data': [
